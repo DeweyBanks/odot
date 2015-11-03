45e38c88e50ffa0105c17117ae6a3fd1588be4ab
Dewey Banks <DeweyBanks@gmail.com>
2015-11-03 11:18:47 -0500

    clean up view

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,6 +1,10 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
 
-  validates :content, presence: true
-  validates :content, length: { minimum: 3 }
+  validates :content, presence: true,
+                      length: { minimum: 3 }
+
+  def completed?
+    !completed_at.blank?
+  end
 end
